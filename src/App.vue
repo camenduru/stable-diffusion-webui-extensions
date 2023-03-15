62b2eebb59d889e22cf16505d9bd13a23ece174a
camenduru <camenduru@gmail.com>
2023-03-15 16:56:58 +0300

    test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { computed, reactive, ref } from "vue";
 import { useFetch } from "@vueuse/core";
-import stable_diffusion_webui_aesthetic_gradients from '/extensions/AUTOMATIC1111/stable-diffusion-webui-aesthetic-gradients/Readme.md'
+import stable_diffusion_webui_aesthetic_gradients from './AUTOMATIC1111_stable_diffusion_webui_aesthetic_gradients.md'
 
 document.title = "Stable Diffusion WebUI Extensions";
 // const url = ref("https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui-extensions/master/index.json");
